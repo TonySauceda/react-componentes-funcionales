54a68538871c53bbe32f53a68c47ee56a957a429
TonySauceda <asauceda@boz.mx>
2021-09-29 15:47:56 -0500

    useRef

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useRef } from 'react'
+
+const App = () => {
+	const ref = useRef()
+	const inputRef = useRef()
+	const click = () => {
+		console.log(ref.current)
+		ref.current.innerHTML = 'Prueba'
+	}
+
+	const focus = () => {
+		inputRef.current.focus()
+	}
+
+	return (
+		<div>
+			<input ref={inputRef} />
+			<button onClick={focus}>Focus</button>
+			<div onClick={click} ref={ref}>
+				Hola Mundo
+			</div>
+		</div>
+	)
+}
+
+export default App
