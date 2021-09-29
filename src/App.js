85cc32837790f55a8832870cf3a9c18d22a7e3a6
TonySauceda <asauceda@boz.mx>
2021-09-29 14:38:52 -0500

    Intro hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,40 @@
+import { Component, useState } from 'react'
+
+// Reglas de los hooks
+// No utilizarlos en loops
+// Siempre utilizarlos en el nivel mas alto del componente
+// Solo se llaman en 2 partes:
+//  1. En componentes de react
+//  2. Custom hooks
+// Cuando creemos un custom hook su nombre debe empezar con 'use'
+
+// Componente basado en clase
+// class App extends Component {
+// 	state = {
+// 		contador: 0,
+// 	}
+// 	render() {
+// 		return (
+// 			<div>
+// 				Contador: {this.state.contador}
+// 				<button onClick={() => this.incrementar(this.state.contador + 1)}>Incrementar</button>
+// 			</div>
+// 		)
+// 	}
+
+// 	incrementar = () => {
+// 		this.setState({ contador: this.state.contador + 1 })
+// 	}
+// }
+
+// Componente funcional
 const App = () => {
-	return 'Hola Mundo'
+	const [contador, setContador] = useState(0)
+	return (
+		<div>
+			Contador: {contador}
+			<button onClick={() => setContador(contador + 1)}>Incrementar</button>
+		</div>
+	)
 }
 export default App
